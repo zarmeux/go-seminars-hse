6301d6e483b42cd919a47d6b2cb27ceb272e8829
Artem Kuznetsov <arankuznetsov@edu.hse.ru>
2025-09-19 17:36:56 +0300

    Up reverse-string

--- src/seminar2/tasks/reverse_string.go
+++ src/seminar2/tasks/reverse_string.go
@@ -1,6 +1,11 @@
 package tasks
 
 // ReverseString переворачивает строку
-func ReverseString(_ string) string {
-	return ""
+func ReverseString(s string) string {
+	new_s := ""
+	for _, char := range s {
+		new_s = string(char) + new_s
+	}
+
+	return new_s
 }

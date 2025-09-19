767c8acd4d496f5506c2b48f2d65c7dd3816e394
Artem Kuznetsov <arankuznetsov@edu.hse.ru>
2025-09-19 17:41:44 +0300

    Up divide

--- src/seminar2/tasks/divide.go
+++ src/seminar2/tasks/divide.go
@@ -1,6 +1,14 @@
 package tasks
 
+import (
+	"fmt"
+)
+
 // Divide выполняет деление двух чисел с обработкой ошибок
 func Divide(a, b float64) (float64, error) {
-	return 0.0, nil
+	if b == 0 {
+		return 0, fmt.Errorf("division by zero")
+	}
+
+	return a / b, nil
 }

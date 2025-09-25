6064a11dc60d6015841604e8efdb7df1881518b4
Artem Kuznetsov <arankuznetsov@edu.hse.ru>
2025-09-25 13:23:12 +0300

    Up filter-numbers

--- src/seminar2/tasks/filter_numbers.go
+++ src/seminar2/tasks/filter_numbers.go
@@ -2,5 +2,12 @@ package tasks
 
 // FilterNumbers фильтрует числа по условию
 func FilterNumbers(numbers []int, predicate func(int) bool) []int {
-	return numbers
+	result := make([]int, 0, len(numbers))
+	for _, number := range numbers {
+		if predicate(number) {
+			result = append(result, number)
+		}
+	}
+
+	return result
 }

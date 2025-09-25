a07a0bc40b3057df2e4dbccf3314e1e1e8a29b2e
Artem Kuznetsov <arankuznetsov@edu.hse.ru>
2025-09-25 13:25:55 +0300

    Up counter

--- src/seminar2/tasks/counter.go
+++ src/seminar2/tasks/counter.go
@@ -1,27 +1,33 @@
 package tasks
 
 type counter struct {
+	value int
 }
 
-func newCounter(_ int) *counter {
-	return &counter{}
+func newCounter(new_value int) *counter {
+	return &counter{value: new_value}
 }
 
 func (c *counter) Increment() {
+	c.value++
 }
 
 func (c *counter) Decrement() {
+	c.value--
 }
 
 func (c *counter) GetValue() int {
-	return 0
+	return c.value
 }
 
 func (c *counter) Reset() {
+	c.value = 0
 }
 
-func (c *counter) Add(_ int) {
+func (c *counter) Add(delta int) {
+	c.value += delta
 }
 
-func (c *counter) Subtract(_ int) {
+func (c *counter) Subtract(delta int) {
+	c.value -= delta
 }
